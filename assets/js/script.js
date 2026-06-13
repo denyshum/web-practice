245f4485e021fdbead15d80e6b4a9d9186a710b4
Denys <genhumenyuk@gmail.com>
2026-06-13 23:57:00 +0300

    Add and implement burger menu
    - Add responsive fullscreen burger menu with smooth animations
    - Add close.svg icon for the open menu state
    - Implement JS logic to toggle the overlay and lock background scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
-console.log('Project started');
+const mainHeaderNav = document.getElementById("main-header__nav");
+const menuBtn = document.getElementById("menu-btn");
+const menuOverlay = document.getElementById("menu-overlay");
+const menuBtnText = document.getElementById("menu-btn__text");
+const menuBtnImg = document.getElementById("menu-btn__img");
+const body = document.body;
+
+menuBtn.addEventListener("click", () => {
+    const isOpen = menuOverlay.classList.toggle("menu-overlay--open");
+    body.classList.toggle("no-scroll");
+
+    if (isOpen) {
+        mainHeaderNav.style.display = "none";
+        menuBtnText.textContent = "Close";
+        menuBtnImg.src = "assets/images/icons/close.svg"
+    } else {
+        mainHeaderNav.style.display = "";
+        menuBtnText.textContent = "Menu";
+        menuBtnImg.src = "assets/images/icons/burger-menu.svg"
+    }
+});
